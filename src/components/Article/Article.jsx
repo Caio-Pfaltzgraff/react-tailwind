9df6cba1a792fc254a44bf367f6b790e8fde5af6
Caio Pfaltzgraff <caio.rllcr@gmail.com>
2023-11-22 14:37:49 -0300

    Pseudo elementos 2

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,9 +1,9 @@
 const Article = ({ title, text, tags, image, alt }) => {
     return (
-        <div className="p-5 bg-gray-200 dark:bg-dark-200 sm:rounded-xl sm:shadow-lg flex gap-2 flex-col items-center">
+        <div className="p-5 bg-gray-200 dark:bg-dark-200 sm:rounded-xl sm:shadow-lg flex gap-2 flex-col items-center sm:hover:shadow-gray-600 sm:hover:dark:shadow-gray-700 sm:hover:shadow-lg">
             <h3 className="text-xl text-azul-200 dark:text-gray-200 font-bold">{title}</h3>
             <div className="w-full flex-row justify-end gap-2 pr-5 hidden sm:flex">
-                {tags.map((tag) => <span className="bg-azul-100 dark:bg-dark-100 px-4 py-1 rounded-full text-gray-200 text-xs font-bold uppercase" key={tag}>{tag}</span>)}
+                {tags.map((tag) => <span className="bg-azul-100 dark:bg-dark-100 px-4 py-1 rounded-full text-gray-200 text-xs font-bold uppercase hover:bg-azul-200 hover:scale-110" key={tag}>{tag}</span>)}
             </div>
             <div className="grid gap-1">
                 {text.map((content, index) => (
